2c0335a5cf8d07e9cea3f71b8af09ba75a6e05b6
KarlaSz <szymaszkiewiczk@gmail.com>
2023-10-31 10:05:37 +0100

    season slider

---
+++ Slider2/main.js
@@ -0,0 +1,20 @@
+const body = document.querySelector('body')
+const cards = document.querySelectorAll('.card')
+
+function showCard() {
+  cards.forEach(card => {
+    /* console.log(this) */
+    card.classList.remove('active')
+    this.classList.add('active')
+  })
+  handleColor(this)
+}
+
+const handleColor = card => {
+  const season = card.getAttribute('data-season')
+  body.setAttribute('data-bg', season)
+}
+
+cards.forEach(card => {
+  card.addEventListener('click', showCard)
+})
